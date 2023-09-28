234dee181720a64441894e03d90d9982086be0ca
micdra01 <114472076+micdra01@users.noreply.github.com>
2023-09-28 15:34:49 +0200

    Encryption, write to file, decrypt from memory

--- Symmetric-Key_Enryption/Program.cs
+++ Symmetric-Key_Enryption/Program.cs
@@ -1,28 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Security.Cryptography;
 using System.Text;
 
-byte[] binary = null;
-string enteredText = "No message has been stored yet";
 bool isA, isB, isC;
-string? password = null;
+string? msgToEncrypt = null;
 
-Login();
+byte[] key = new byte[32];
+byte[] nonce = new byte[AesGcm.NonceByteSizes.MaxSize];
+byte[] plaintextBytes = new byte[32];
+byte[] ciphertext = new byte[plaintextBytes.Length];
+byte[] tag = new byte[AesGcm.TagByteSizes.MaxSize];
 
+//Attempt to include passphrase to read from file in a later session
+string? passphrase = "dummy";
+byte[] Salt = new byte[10];
+
+
+RunProgram();
+
+//Unused attempt to include passphrase in order to read from the file in a later session
 void Login()
 {
-    Console.WriteLine("Passphrase:");
-    password = Console.ReadLine();
+    Console.WriteLine("Enter your passphrase:");
+    passphrase = Console.ReadLine();
     
-    if (password.Equals("pw"))
-    {
-        RunProgram();
-    }
-    else
-    {
-        Console.WriteLine("Wrong password. (hint, it's 'pw')");
-        Login();
-    }
+    RunProgram();
 }
 
 void RunProgram()
@@ -49,7 +52,15 @@ void ReadInput()
     }
     else if (isB)
     {
-        DecryptMessage();
+        if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/EncryptedMsg.txt"))
+        {
+            DecryptMessage();
+        }
+        else
+        {
+            Console.WriteLine("No message to decrypt found");
+            RunProgram();
+        }
     }
     else if (isC)
     {
@@ -66,24 +77,78 @@ void ReadInput()
 void EncryptMessage()
 {
     Console.WriteLine("Enter your message to encrypt");
+    msgToEncrypt = Console.ReadLine();
 
-    binary = Encoding.UTF8.GetBytes(Console.ReadLine());
-        
+    //Key generation using passphrase
+    key = CreateKey(passphrase);
+    using var aes = new AesGcm(key);
+    
+    //'Number used once'/IV generation
+    RandomNumberGenerator.Fill(nonce);
+    
+    //Plaintext bytes and ciphertext preparation
+    plaintextBytes = Encoding.UTF8.GetBytes(msgToEncrypt);
+    ciphertext = new byte[plaintextBytes.Length];
+    
+    //Encrypting
+    aes.Encrypt(nonce, plaintextBytes, ciphertext, tag);
     Console.WriteLine("Message encrypted.");
+    
+    // Write to a file
+    byte[] combined = new byte[nonce.Length + ciphertext.Length + tag.Length];
+    System.Buffer.BlockCopy(nonce, 0, combined, 0, nonce.Length);
+    System.Buffer.BlockCopy(ciphertext, 0, combined, nonce.Length, ciphertext.Length);
+    System.Buffer.BlockCopy(tag, 0, combined, nonce.Length + ciphertext.Length, tag.Length);
+    File.WriteAllBytes(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/EncryptedMsg.txt", combined);
+    
     RunProgram();
 }
 
 void DecryptMessage()
 {
-    Console.WriteLine("Here is the decrypted message: ");
-
-    if (binary == null)
+    //Show encrypted message from file
+    using (var sr = new StreamReader(
+               Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/EncryptedMsg.txt"))
     {
-        Console.WriteLine(enteredText);
+        Console.WriteLine("Here is the message as read from the file: ");
+        Console.WriteLine(sr.ReadToEnd());
     }
-    else
+
+    //Decrypt the message from memory, can't work it out from the file :(
+    try
     {
-        Console.WriteLine(Encoding.UTF8.GetString(binary));
+        key = CreateKey(passphrase);
+        using (var aes = new AesGcm(key))
+        {
+            Console.WriteLine("Here is the decrypted message: ");
+            plaintextBytes = new byte[ciphertext.Length];
+        
+            aes.Decrypt(nonce, ciphertext, tag, plaintextBytes);
+
+            Console.WriteLine(Encoding.UTF8.GetString(plaintextBytes));;
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        RunProgram();
     }
     RunProgram();
 }
+
+byte[] CreateKey(string passphrase, int keyBytes = 32)
+{
+    const int Iterations = 300;
+    var keyGenerator = new Rfc2898DeriveBytes(passphrase, Salt, Iterations);
+    return keyGenerator.GetBytes(keyBytes);
+}
+
+
+// ReSharper disable once InvalidXmlDocComment
+/**
+ * Notes:
+ *
+ * SecretMessage.cs med 4 x byte[], Nonce, Tag, Salt, Cipher
+ * JSON serialize det i en JsonStorage.cs ?
+ * Så kan json tingen skrives til fil og læses igen
+*/
